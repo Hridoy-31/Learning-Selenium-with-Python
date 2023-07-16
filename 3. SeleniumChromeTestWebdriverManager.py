e7fffe7267e629c70eee0f10c9809abfe98e4924
S.M. Raihanul Bashir Hridoy <raihanulbashirhridoy@gmail.com>
2023-07-16 14:31:53 +0600

    Testing Selenium with the help of Webdriver Manager

---
+++ 3. SeleniumChromeTestWebdriverManager.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+# importing ChromeDriverManager() from webdriver_manager
+# it will manage Chrome driver automatically with the
+# correspoding Chrome browser version.
+from webdriver_manager.chrome import ChromeDriverManager
+
+# replacing the executable_path with the ChromeDriverManager()
+# for automatically check the version and install.
+# No need for locally installed chrome driver
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("https://www.github.com")
+print(driver.title)
+driver.maximize_window()
+driver.close()
